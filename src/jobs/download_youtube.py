503d79a236e833d282a42bc3542655250b119b02
Diaszano <lucasdiiassantos@gmail.com>
2022-07-23 01:40:43 -0300

    projeto feito, mas ainda precisando de ajustes!

--- src/jobs/download_youtube.py
+++ src/jobs/download_youtube.py
@@ -2,15 +2,20 @@
 # BIBLIOTECAS
 #-----------------------
 import os
+import re
+import asyncio
+import zipfile
 from time import sleep
 import moviepy.editor as mp
 from datetime import datetime
-from typing import List, Union
+from src.schemas import schemas
 from pytube import Playlist, YouTube
+from typing import List, NoReturn, Union
 from src.jobs.check_internet import CheckInternet
 #-----------------------
 # CONSTANTES
 #-----------------------
+NUMERO_MAXIMO_CONCORRENCIA:int = 10;
 #-----------------------
 # CLASSES
 #-----------------------
@@ -30,11 +35,11 @@ class DownloadYouTube:
         Args:
             dias     (int, optional): Dias para remoção dos 
             arquivos baixados.
-            horas    (int, optional): Dias para remoção dos 
+            horas    (int, optional): Horas para remoção dos 
             arquivos baixados.
-            minutos  (int, optional): Dias para remoção dos 
+            minutos  (int, optional): Minutos para remoção dos 
             arquivos baixados.
-            segundos (int, optional): Dias para remoção dos 
+            segundos (int, optional): Segundos para remoção dos 
             arquivos baixados.
         """
         self.__dias    :int = dias;
@@ -42,19 +47,33 @@ class DownloadYouTube:
         self.__minutos :int = minutos;
         self.__segundos:int = segundos;
         if((dias+horas+minutos+segundos) <= 0):
-            self.__tempo_total:int = 1*60;
+            self.tempo_total:int = 1*60;
         else:
-            self.__tempo_total:int = (
+            self.tempo_total:int = (
                 (self.__segundos) +
                 (60*self.__minutos) + 
                 (60*60*self.__horas) + 
                 (60*60*24*self.__dias)
             );
-            if(self.__tempo_total <= 0):
-                self.__tempo_total:int = 1*60;
+            if(self.tempo_total <= 0):
+                self.tempo_total:int = 1*60;
         self.__check_internet = CheckInternet();
+    
+    @staticmethod
+    def data() -> str:
+        """Data
         
-    def remover(self,caminho_do_arquivo:str) -> None:
+        Aqui nós pegaremos a data atual.
+
+        Returns:
+            str: retorna a data neste formato.
+                Dia-Mês-Ano_Hora-Minutos-Segundos
+        """
+        now = datetime.now();
+        time = now.strftime("%d-%m-%Y_%H:%M:%S");
+        return time;
+    
+    def remover(self,caminho_do_arquivo:str) -> NoReturn:
         """Remover
 
         Neste método iremos remover o arquivo baixado anteriormente.
@@ -62,13 +81,14 @@ class DownloadYouTube:
         Args:
             caminho_do_arquivo (str): Caminho do arquivo baixado.
         """
-        sleep(self.__tempo_total);
+        sleep(self.tempo_total);
         if(os.path.exists(caminho_do_arquivo)):
             os.remove(caminho_do_arquivo);
         if(os.path.exists(os.path.dirname(caminho_do_arquivo))):
             os.rmdir(os.path.dirname(caminho_do_arquivo));
     
-    def __remover_arquivo(self,caminho_do_arquivo:str) -> None:
+    @staticmethod
+    def __remover_arquivo(caminho_do_arquivo:str) -> NoReturn:
         """Remover
 
         Neste método iremos remover o arquivo baixado anteriormente.
@@ -78,6 +98,18 @@ class DownloadYouTube:
         """
         if(os.path.exists(caminho_do_arquivo)):
             os.remove(caminho_do_arquivo);
+    
+    @staticmethod
+    def __remover_pasta(caminho_da_pasta:str) -> NoReturn:
+        """Remover
+
+        Neste método iremos remover uma pasta criada anteriormente.
+        
+        Args:
+            caminho_do_arquivo (str): Caminho do arquivo baixado.
+        """
+        if(os.path.exists(caminho_da_pasta)):
+            os.rmdir(caminho_da_pasta);
 
     async def __verificacao(self,link:str) -> Union[None,str]:
         """Verificação
@@ -104,7 +136,33 @@ class DownloadYouTube:
         
         return url;
     
-    async def baixar_video(self,link:str) -> List[Union[str,None]]:
+    async def __pegar_video(self,link:str) -> Union[YouTube,str,None]:
+        # Retorno padrão
+        retorno:list = [None,None];
+        # Verificar se é playlist
+        verificado = re.findall(
+            r"(/watch\?v=[\w-]*)",
+            link
+        );
+        if(not verificado):
+            return retorno;
+        # Verificar a conexão com o link
+        url = await self.__verificacao(link);
+        if(not url):
+            return retorno;
+        # Pegar a playlist
+        youtube = YouTube(url);
+        titulo:str = youtube.title;
+        video = youtube.streams.filter(
+            progressive=True, 
+            file_extension='mp4'
+        ).order_by(
+            'resolution'
+        ).desc().first();
+        
+        return video,titulo;
+    
+    async def baixar_video(self,link:str,pasta:str=None) -> List[Union[str,None]]:
         """Baixar Video
 
         Args:
@@ -115,25 +173,18 @@ class DownloadYouTube:
             List[str|None]: Aqui retornaremos em primeiro o título do 
             video e por segundo o seu caminho ou Nulo.
         """
-        url = await self.__verificacao(link=link);
-        
-        if(not url):
-            return None, None;
-        
-        youtube = YouTube(url);
-        
-        video = youtube.streams.filter(
-            progressive=True, 
-            file_extension='mp4'
-        ).order_by(
-            'resolution'
-        ).desc().first();
-        now = datetime.now();
-        time = now.strftime("%d-%m-%Y_%H:%M:%S")
-        caminho_video = video.download(f"./data/video/{time}");
-        return youtube.title,caminho_video;
+        # Verifica se precisa por em uma pasta especifica.
+        if(not pasta):
+            pasta = f"./data/video/{self.data()}";
+        # Pega o video do YouTube
+        [youtube,titulo] = await self.__pegar_video(link);
+        youtube: YouTube;
+        titulo : str;
+        # Baixa o video
+        caminho_video = youtube.download(pasta);
+        return titulo,caminho_video;
     
-    async def baixar_musica(self,link:str) -> List[Union[str,None]]:
+    async def baixar_musica(self,link:str,pasta:str=None) -> List[Union[str,None]]:
         """Baixar música
 
         Args:
@@ -144,27 +195,19 @@ class DownloadYouTube:
             List[str|None]: Aqui retornaremos em primeiro o título da 
             música e por segundo o seu caminho ou Nulo.
         """
-        url = await self.__verificacao(link=link);
-        
-        if(not url):
-            return None, None;
-        
-        youtube = YouTube(url);
-        
-        video = youtube.streams.filter(
-            progressive=True, 
-            file_extension='mp4'
-        ).order_by(
-            'resolution'
-        ).desc().first();
-        
-        now = datetime.now();
-        time = now.strftime("%d-%m-%Y_%H:%M:%S")
-        
-        caminho_video = video.download(f"./data/music/{time}");
+        # Verifica se precisa por em uma pasta especifica.
+        if(not pasta):
+            pasta = f"./data/music/{self.data()}";
+        # Pega o video do YouTube
+        [youtube,titulo] = await self.__pegar_video(link);
+        youtube: YouTube;
+        titulo : str;
+        # Baixa o video
+        caminho_video = youtube.download(pasta);
+        # Transforma em música
         caminho_musica = await self.converter_to_mp3(caminho_video);
         
-        return youtube.title,caminho_musica;
+        return titulo,caminho_musica;
     
     async def converter_to_mp3(self,arq_video:str):
         name, ext = os.path.splitext(arq_video);
@@ -175,6 +218,99 @@ class DownloadYouTube:
         
         self.__remover_arquivo(arq_video);
         return out_name;
+    
+    async def __pegar_playlist(self,link:str) -> Union[Playlist,str,None]:
+        # Retorno padrão
+        retorno:list = [None,None];
+        # Verificar se é playlist
+        verificado = re.findall(
+            r"(/playlist\?list=[\w-]*)",
+            link
+        );
+        if(not verificado):
+            return retorno;
+        # Verificar a conexão com o link
+        url = await self.__verificacao(link);
+        if(not url):
+            return retorno;
+        # Pegar a playlist
+        playlist   = Playlist(url);
+        titulo:str = playlist.title;
+        
+        return playlist,titulo;
+    
+    def __zipar_playlist(self,pasta:str,arquivo_zip:str) -> NoReturn:
+        # Pega os arquivos
+        arquivos = (
+            arquivo
+            for _, _, arquivos in os.walk(os.path.abspath(pasta))
+                for arquivo in arquivos
+                    if(".mp" in arquivo)
+        );
+        # Cria o arquivo zip
+        with zipfile.ZipFile(arquivo_zip, 'w', zipfile.ZIP_DEFLATED) as zipar:
+            for arquivo in arquivos:
+                zipar.write(f"{pasta}/{arquivo}",arquivo);
+                self.__remover_arquivo(f"{pasta}/{arquivo}");
+        self.__remover_pasta(pasta);
+    
+    @staticmethod
+    async def __execute_coroutines(tasks:list,max:int = 10) -> NoReturn:
+        dltasks = set();
+        for task in tasks:
+            if(len(dltasks) >= max):
+                _done, dltasks = await asyncio.wait(
+                    dltasks, 
+                    return_when=asyncio.FIRST_COMPLETED
+                );
+            dltasks.add(asyncio.create_task(task));
+        await asyncio.wait(dltasks);
+    
+    async def baixar_playlist_videos(self,link:str) -> str:
+        retorno:str = None,None;
+        # Pegar a playlist
+        [playlist,titulo] = await self.__pegar_playlist(link);
+        playlist: Playlist;
+        titulo  : str;
+        if((not titulo)or(not playlist)):
+            return retorno;
+        # Cria o caminho dos arquivos
+        pasta_primaria:str = f"./data/playlist/video/{self.data()}";
+        arquivo_zip   :str = f'{pasta_primaria}/{titulo}.zip';
+        pasta         :str = f"{pasta_primaria}/{titulo}";
+        # Cria as sub-rotinas
+        tasks = (
+            self.baixar_video(video,pasta)
+            for video in playlist.video_urls
+        )
+        # Executa as sub-rotinas
+        await self.__execute_coroutines(tasks);
+        # Zipar os arquivos
+        self.__zipar_playlist(pasta,arquivo_zip);
+        return titulo,arquivo_zip;
+    
+    async def baixar_playlist_musicas(self,link:str) -> str:
+        retorno:str = None,None;
+        # Pegar a playlist
+        [playlist,titulo] = await self.__pegar_playlist(link);
+        playlist: Playlist;
+        titulo  : str;
+        if((not titulo)or(not playlist)):
+            return retorno;
+        # Cria o caminho dos arquivos
+        pasta_primaria:str = f"./data/playlist/music/{self.data()}";
+        arquivo_zip   :str = f'{pasta_primaria}/{titulo}.zip';
+        pasta         :str = f"{pasta_primaria}/{titulo}";
+        # Cria as sub-rotinas
+        tasks = (
+            self.baixar_musica(video,pasta)
+            for video in playlist.video_urls
+        )
+        # Executa as sub-rotinas
+        await self.__execute_coroutines(tasks);
+        # Zipar os arquivos
+        self.__zipar_playlist(pasta,arquivo_zip);
+        return titulo,arquivo_zip;
 #-----------------------
 # FUNÇÕES()
 #-----------------------
--- src/routers/router_youtube.py
+++ src/routers/router_youtube.py
@@ -2,9 +2,8 @@
 #-----------------------
 # BIBLIOTECAS
 #-----------------------
-from typing import List
-from fastapi.responses import (
-    StreamingResponse, FileResponse
+from fastapi.responses import ( 
+    FileResponse
 )
 from fastapi import (
     APIRouter, BackgroundTasks, status, HTTPException
@@ -14,13 +13,14 @@ from src.jobs.download_youtube import DownloadYouTube
 # CONSTANTES
 #-----------------------
 router = APIRouter(prefix="/youtube");
+download_youtube = DownloadYouTube();
 #-----------------------
 # CLASSES
 #-----------------------
 #-----------------------
 # FUNÇÕES()
 #-----------------------
-@router.get("/download/video/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Baixar"])
+@router.get("/download/video/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Video"])
 async def download_video(link:str,background_tasks: BackgroundTasks):
     """Baixar o video
 
@@ -30,9 +30,20 @@ async def download_video(link:str,background_tasks: BackgroundTasks):
         MP4: retorna o video.
     """
     try:
-        nome,arquivo = await DownloadYouTube().baixar_video(link);
+        nome,arquivo = await download_youtube.baixar_video(link);
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+    if((not nome)or(not arquivo)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Requisição incorreta"
+    );
+    try:
         retorno = FileResponse(arquivo,media_type=".mp4",filename=f"{nome}.mp4");
-        background_tasks.add_task(DownloadYouTube().remover,arquivo);
+        background_tasks.add_task(download_youtube.remover,arquivo);
         return retorno;
     except Exception as error:
         raise HTTPException(
@@ -40,19 +51,71 @@ async def download_video(link:str,background_tasks: BackgroundTasks):
             detail=f"{error}"
         );
 
-@router.get("/download/musica/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Baixar"])
+@router.get("/download/musica/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Música"])
 async def download_musica(link:str,background_tasks: BackgroundTasks):
-    """Baixar a música
-
-    Args:
-        link (str): Deve ser um link existente no youtube.
-    Returns:
-        MP4: retorna a música.
-    """
     try:
-        nome,arquivo = await DownloadYouTube().baixar_musica(link);
+        nome,arquivo = await download_youtube.baixar_musica(link);
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+    if((not nome)or(not arquivo)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Requisição incorreta"
+    );
+    try:
         retorno = FileResponse(arquivo,media_type=".mp3",filename=f"{nome}.mp3");
-        background_tasks.add_task(DownloadYouTube().remover,arquivo);
+        background_tasks.add_task(download_youtube.remover,arquivo);
+        return retorno;
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+
+@router.get("/download/video/playlist/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Playlist","Video"])
+async def download_playlist_videos(link:str,background_tasks: BackgroundTasks):
+    try:
+        nome,arquivo = await download_youtube.baixar_playlist_videos(link);
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+    if((not nome)or(not arquivo)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Requisição incorreta"
+    );
+    try:
+        retorno = FileResponse(arquivo,media_type=".zip",filename=f"{nome}.zip");
+        background_tasks.add_task(download_youtube.remover,arquivo);
+        return retorno;
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+
+@router.get("/download/musica/playlist/",response_class=FileResponse,status_code=status.HTTP_200_OK,tags=["Playlist","Música"])
+async def download_playlist_musicas(link:str,background_tasks: BackgroundTasks):
+    try:
+        nome,arquivo = await download_youtube.baixar_playlist_musicas(link);
+    except Exception as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{error}"
+        );
+    if((not nome)or(not arquivo)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Requisição incorreta"
+    );
+    try:
+        retorno = FileResponse(arquivo,media_type=".zip",filename=f"{nome}.zip");
+        background_tasks.add_task(download_youtube.remover,arquivo);
         return retorno;
     except Exception as error:
         raise HTTPException(
